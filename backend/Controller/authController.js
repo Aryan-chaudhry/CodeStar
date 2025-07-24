522c737e9a3b4ac93f225778e8745e7795ef781b
Aryan <Aryanchaudhary2629@gmail.com>
2025-07-24 18:18:22 +0530

    Testing Signup

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -1,7 +1,41 @@
 const User = require('../Model/User');
 const bcrypt = require('bcryptjs');
 
-const signup = async (req, res) => {
-    const {name, email, password} = req.body;
-    console.log(name, email, password);
+exports.signup = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        console.log(name, email, password);
+
+        if(name !== null && email !== null && password !== null){
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        // check if user already exist 
+        const existingUser = User.findOne({email});
+        if(existingUser){
+            return res.status(409).json({message: "User All ready exists"});
+        }
+
+        // this is new user this user is not exist 
+        // so lets insert it now
+
+        // but first hash the password
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        // create new user
+
+        const newUser = new User({
+            name,
+            email,
+            password:hashedPassword,
+        });
+
+        // save user
+
+        await newUser.save();
+        console.log("User Added Successfully");
+    } 
+    catch (error) {
+        console.log("SignUp error", error);
+    }
 }
